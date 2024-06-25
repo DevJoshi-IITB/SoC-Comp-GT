37e087368c1ff69f5e8176890e36095795f4d121
DevJoshi-IITB <144384559+DevJoshi-IITB@users.noreply.github.com>
2024-06-25 19:36:45 +0530

    Create 01matrix.py

---
+++ 01matrix.py
@@ -0,0 +1,42 @@
+'''
+Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
+
+The distance between two adjacent cells is 1.
+
+ 
+Example 1:
+Input: mat = [[0,0,0],[0,1,0],[0,0,0]]
+Output: [[0,0,0],[0,1,0],[0,0,0]]
+
+Example 2:
+Input: mat = [[0,0,0],[0,1,0],[1,1,1]]
+Output: [[0,0,0],[0,1,0],[1,2,1]]
+'''
+
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        if not mat or not mat[0]:
+            return []
+
+        m, n = len(mat), len(mat[0])
+        queue = deque()
+        MAX_VALUE = m * n
+      
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 0:
+                    queue.append((i, j))
+                else:
+                    mat[i][j] = MAX_VALUE
+        
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        
+        while queue:
+            row, col = queue.popleft()
+            for dr, dc in directions:
+                r, c = row + dr, col + dc
+                if 0 <= r < m and 0 <= c < n and mat[r][c] > mat[row][col] + 1:
+                    queue.append((r, c))
+                    mat[r][c] = mat[row][col] + 1
+        
+        return mat
